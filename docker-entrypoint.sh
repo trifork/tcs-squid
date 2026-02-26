bc279185b39b6ee8a766ba93b5259f1cbc770616
Adam Zilizi <adzi@trifork.com>
2026-02-26 05:23:18 +0100

    Add docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+# Forward common Squid logs to container logs.
+tail -F /var/log/squid/access.log 2>/dev/null &
+tail -F /var/log/squid/error.log 2>/dev/null &
+tail -F /var/log/squid/store.log 2>/dev/null &
+tail -F /var/log/squid/cache.log 2>/dev/null &
+
+# Create missing cache directories and exit.
+/usr/sbin/squid -Nz
+
+# Run Squid with CMD/command arguments.
+exec /usr/sbin/squid "$@"
